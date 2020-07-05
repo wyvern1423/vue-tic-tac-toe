e22f4d754e7fca8b9a58e3f7d68192678e09387e
wyvern1423 <xiaoxun1208@126.com>
2020-07-05 11:51:52 +0800

    create

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="row">
+      <Cell :value="clickNum" @click-num-change="clickCount(0,$event)"></Cell>
+      <Cell :value="clickNum" @click-num-change="clickCount(1,$event)"></Cell>
+      <Cell :value="clickNum" @click-num-change="clickCount(2,$event)"></Cell>
+    </div>
+    <div class="row">
+      <Cell :value="clickNum" @click-num-change="clickCount(3,$event)"></Cell>
+      <Cell :value="clickNum" @click-num-change="clickCount(4,$event)"></Cell>
+      <Cell :value="clickNum" @click-num-change="clickCount(5,$event)"></Cell>
+    </div>
+    <div class="row">
+      <Cell :value="clickNum" @click-num-change="clickCount(6,$event)"></Cell>
+      <Cell :value="clickNum" @click-num-change="clickCount(7,$event)"></Cell>
+      <Cell :value="clickNum" @click-num-change="clickCount(8,$event)"></Cell>
+    </div>
+    {{result}}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Cell from "./components/Cell";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {Cell},
+    data() {
+      return {
+        clickNum: 0,
+        map: [
+          [null, null, null], [null, null, null], [null, null, null]
+        ],
+        result: false
+      }
+    },
+    methods: {
+      clickCount(i, val) {
+        this.clickNum = val[0]
+        this.map[Math.floor(i / 3)][i % 3] = val[1]
+        console.log(this.map)
+        console.log(`text和val分别是${val[0]}+${val[1]}`)
+        this.tell()
+      },
+      tell() {
+        const map = this.map
+        for (let i = 0; i < 3; i++) {
+          if (map[i][0] !== null && map[i][0] === map[i][1] && map[i][1] === map[i][2]) {
+            this.result = true
+          }
+        }
+        for (let i = 0; i < 3; i++) {
+          if (map[0][i] !== null && map[0][i] === map[1][i] && map[1][i] === map[2][i]) {
+            this.result = true
+          }
+        }
+        if (map[0][0] !== null && map[1][1] === map[2][2] && map[1][1] === map[0][0]){
+          this.result =true
+        }
+        if (map[0][2] !== null && map[0][2] === map[1][1] && map[1][1] === map[2][0]) {
+          this.result = true
+        }
+      }
+    }
   }
-}
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .row {
+    display: flex;
+  }
 </style>
---
+++ src/components/Cell.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="cell" @click="xClick">
+    <template v-if="xDisplay">{{text}}</template>
+    <template v-else></template>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'Cell',
+    props:['value'],
+    data() {
+      return {
+        xDisplay: false,
+        text:null
+      }
+    },
+    methods: {
+      xClick() {
+        if (this.text !== null){
+          return
+        }
+        if ((this.value+1)%2 ===1){
+          this.text='o'
+        }else {
+          this.text='x'
+        }
+        this.xDisplay = true
+        this.$emit('click-num-change',[this.value+1,this.text])
+      }
+    }
+  }
+
+</script>
+
+<style>
+  .cell {
+    font-size: 24px;
+    border: 1px solid black;
+    width: 50px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
